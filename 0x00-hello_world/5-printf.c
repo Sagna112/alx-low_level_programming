62023930af49529fdd3e453e9b4ea0ad3815d7e7
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-08 07:25:24 -0700

    My C commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,8 @@
  *
  * return : 0 always
  */
-int main(void)
+int main()
 {
-	printf("\"with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
