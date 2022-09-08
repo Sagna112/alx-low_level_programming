e20e602a8543fd883706a265b1e420e2c1ef3e25
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-08 10:13:37 -0700

    My C commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry option
  *
@@ -8,6 +10,6 @@
  */
 int main(void)
 {
-	gets("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return 1;
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
