3f02a795ea45f0a475741e6c014bb75be4236d7c
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-08 06:07:14 -0700

    My C commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main
+ * decription
+ * prints "Programming is like building a multilingual puzzle"
+ *Return: Always 0
+ *
+ */
 int main(void)
 {
         puts("Programming is like building a multilingual puzzle\n");
