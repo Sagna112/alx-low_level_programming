d7d0575e6d2938573c2215047a75b3a20193e8ce
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-09 05:45:38 -0700

    C programming commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+	/**
+	 * main -Entry point
+	 * /
+	 int n = 0;
+	 /* C condition statement */
+	if( n > 0)
+{ printf("is positive\n");}
+          else if( n == 0) {
+	printf("is zero\n");
+	/* it should return zero */
+	  else {/* it should return its negative */  printf("is negative\n");}
+	  return (0);
