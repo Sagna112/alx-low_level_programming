871e6069774a6c339025d570c951b9e90d6e4c14
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-13 08:58:11 -0700

    C function commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - alphabet in lower
+ *
+ * Deacription - print alphabet
+ *
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	int i = 97;
+       while (i < 123)
+       {
+	_putchar(i);
+	_putchar('\n');
+       }	
+
+
+
+
+	return 0;
