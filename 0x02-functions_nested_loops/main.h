62726efd0f6e34ef658a2ad723cd3b05c1cdf354
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-13 07:42:51 -0700

    C function commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,11 @@
-int _putchar(char c)
-	return 0;
+#include <unistd.h>
+
+/**
+*_putchar - writes the character c to stdout
+*@c: The character to print
+*
+*Return: In success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _putchar(char c);
