688be8db32591a444a1a9b5f05cb87feed4b0a3c
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-13 09:34:50 -0700

    C function commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,10 @@
  */
 void print_alphabet(void);
 int main(void)
-{	print_alphabet();
+{	
 	int i = 97;
-       while (i < 123)
+	while (i < 123)
+	print_alphabet();
        {
 	_putchar(i);
 	_putchar('\n');
