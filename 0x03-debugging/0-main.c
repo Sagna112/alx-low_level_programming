cdf99bcb15b9477ee273313c155c7dfbc200af9c
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-14 23:55:28 -0700

    Debugging commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,23 +4,12 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
-void positive_or_negative(int i)
+int main(void)
 {
         int i;
 
-        i = 98;
-	if (i > 0)
-	{ 
-		printf("%i is positive\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else
-	{
-		printf("%d is zero\n", i);
+        i = 0;
+        positive_or_negative(i);
 
         return (0);
 }
