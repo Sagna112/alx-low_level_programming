19a60048459604c8b650a68d3b8622ea135a2f4b
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-15 07:59:01 -0700

    More loop commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _isupper(int c) - print upper
+ *
+ * Return 1 
+ */
+int _isupper(int c)
+{
+	char c;
+	if (c >= 'A' && 'Z' )
+	{
+		return (1);
+	}
+	else
+		return 0;
+}
