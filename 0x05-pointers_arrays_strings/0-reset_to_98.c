d92d9f0a16398a11d2b8d0b16a9c26b321c293c2
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-19 06:47:08 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - check the code for pointer
- *@n - n is pointer n and return value 98
+ * @n : n is pointer n and return value 98
  * Return void
  */
 void reset_to_98(int *n)
