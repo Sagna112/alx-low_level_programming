c2483759dd285e6243c15793ca122d3ac0f0c297
user.name <Sagna112.ridwanullahiolayemi@gmail.com>
2022-09-28 16:28:41 -0700

    C recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: character of a string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
